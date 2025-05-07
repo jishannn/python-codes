05eb8f5f971e31640585c24c4642b61007b0ff13
6ezishan <iamzishan083@gmail.com>
2025-05-07 11:23:49 +0600

    initial commit

--- dsa-python/app.py
+++ dsa-python/app.py
@@ -39,3 +39,82 @@ def selection_sort(selection_arr):
     return selection_arr
 
 print(selection_sort(selection_arr))
+
+
+# Insertion Sort
+""" Insertion Sort builds the sorted array one element at a time by moving each new element into its correct position. """
+
+
+insertion_arr = [100, 60, 30, 18, 10, 90, 12]
+
+def insertion_sort(insertion_arr):
+    for i in range(1, len(insertion_arr)):
+        key = insertion_arr[i]
+        m = i - 1
+
+        # It would move elements of selection_arr[0...i-1] that are greater than key.
+        while m >= 0 and insertion_arr[m] > key:
+            insertion_arr[m + 1] = insertion_arr[m]
+            m -= 1
+        insertion_arr[m + 1] = key
+    return insertion_arr
+
+print(insertion_sort(insertion_arr))
+
+
+# Merge Sort
+""" Merge Sort is a divide and conquer algorithm. It divides the array into halves, recursively sorts them, and merges them back together. """
+
+merge_arr = [90, 50, 30, 20, 10, 80, 12]
+
+def merge_sort(merge_arr):
+    if len(merge_arr) > 1:
+        mid = len(merge_arr) // 2
+        left_half = merge_arr[:mid]
+        right_half = merge_arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = p = s = 0   # i for left half, p for right half, s for original array index.
+        while i < len(left_half) and p < len(right_half):
+            if left_half[i] < right_half[p]:
+                merge_arr[s] = left_half[i]
+                i += 1
+            else:
+                merge_arr[s] = right_half[p]
+                p += 1
+            s += 1
+
+        while i < len(left_half):
+            merge_arr[s] = left_half[i]
+            i += 1
+            s += 1
+        
+        while p < len(right_half):
+            merge_arr[s] = right_half[p]
+            p += 1
+            s += 1
+
+    return merge_arr
+
+print(merge_sort(merge_arr))
+
+
+# Quick Sort
+""" Quick Sort is another divide and conquer algorithm. It selects a 'pivot' element and partitions the array around the pivot. """
+
+quick_arr = [10, 12, 20, 30, 50, 85, 80]
+
+def quick_sort(quick_arr):
+    if len(quick_arr) <= 1:
+        return quick_arr
+    
+    pivot = quick_arr[len(quick_arr) // 2]
+    left = [x for x in quick_arr if x < pivot]
+    middle = [x for x in quick_arr if x == pivot]
+    right = [x for x in quick_arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+print(quick_sort(quick_arr))
